a4cdb29f7b53091da8d9b790b8896409e105a479
Earl Harris <esharris@mac.com>
2023-01-24 09:55:04 -0500

    Lesson 128

--- src/main/java/com/in28minutes/microservices/limitsservice/configuration/Configuration.java
+++ src/main/java/com/in28minutes/microservices/limitsservice/configuration/Configuration.java
@@ -2,23 +2,20 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.in28minutes.microservices.limitsservice.bean;
+package com.in28minutes.microservices.limitsservice.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 /**
  *
  * @author earlharris
  */
-public class Limits {
+@Component
+@ConfigurationProperties("limits-service")
+public class Configuration {
     private int minimum;
     private int maximum;
-
-    public Limits() {
-    }
-
-    public Limits(int minimum, int maximum) {
-        this.minimum = minimum;
-        this.maximum = maximum;
-    }
 
     public int getMinimum() {
         return minimum;
--- src/main/java/com/in28minutes/microservices/limitsservice/controller/LimitsController.java
+++ src/main/java/com/in28minutes/microservices/limitsservice/controller/LimitsController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.in28minutes.microservices.limitsservice.bean.Limits;
+import com.in28minutes.microservices.limitsservice.configuration.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 /**
  *
  * @author earlharris
@@ -15,8 +17,12 @@ import com.in28minutes.microservices.limitsservice.bean.Limits;
 @RestController
 public class LimitsController {
     
+    @Autowired
+    private Configuration configuration;
+    
     @GetMapping("/limits")
     public Limits retrieveLimits() {
-        return new Limits(1, 1000);
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
+//        return new Limits(1, 1000);
     }
 }
